8374a0684b87f7d9a84c5af35f06be672f6c0428
Adrian Irving-Beer <wisq@wisq.net>
2019-02-09 02:03:16 -0500

    Refactor Server to use `handle_continue` to process channels and determine timeout.

--- lib/dyn_chan/server.ex
+++ lib/dyn_chan/server.ex
@@ -110,8 +110,7 @@ defmodule DynChan.Server do
     state = %State{state | category_id: category_id, channels: channels}
 
     log(:info, state, "Now monitoring; ID: #{inspect(id)}")
-    {next, state} = process_channels(state)
-    {:ok, state, next}
+    {:ok, state, {:continue, nil}}
   end
 
   @impl true
@@ -122,8 +121,7 @@ defmodule DynChan.Server do
   @impl true
   def handle_call({:create, name}, _from, state) do
     {rval, state} = handle_create_channel(name, state)
-    {next, state} = process_channels(state)
-    {:reply, rval, state, next}
+    {:reply, rval, state, {:continue, nil}}
   end
 
   defp handle_create_channel(name, %State{category_id: nil} = state) do
@@ -170,22 +168,19 @@ defmodule DynChan.Server do
 
   @impl true
   def handle_cast(:poke, state) do
-    {next, state} = process_channels(state)
-    {:noreply, state, next}
+    {:noreply, state, {:continue, nil}}
   end
 
   @impl true
   def handle_info(:timeout, state) do
-    {next, state} = process_channels(state)
-    {:noreply, state, next}
+    {:noreply, state, {:continue, nil}}
   end
 
   @impl true
   def handle_continue(channel_id, state) do
-    state = delete_channel(state, channel_id)
-
-    {next, state} = process_channels(state)
-    {:noreply, state, next}
+    state = update_channel_timeouts(state)
+    {timeout, state} = process_next_channel(state)
+    {:noreply, state, timeout}
   end
 
   defp init_dynamic_channels(id, state) do
@@ -218,48 +213,47 @@ defmodule DynChan.Server do
     |> Enum.map(&Channel.from_discord/1)
   end
 
-  defp process_channels(state) do
+  defp update_channel_timeouts(state) do
     active =
       VoiceStates.get_active_channels(state.id)
       |> MapSet.new()
 
     channels = Enum.map(state.channels, &Channel.update(&1, active))
-    state = %State{state | channels: channels}
-
-    {calculate_next(state), state}
+    %State{state | channels: channels}
   end
 
-  defp calculate_next(%State{channels: []} = state) do
+  defp process_next_channel(%State{channels: []} = state) do
     log(:info, state, "No dynamic channels.")
-    @default_timeout
+    {@default_timeout, state}
   end
 
-  defp calculate_next(state) do
+  defp process_next_channel(state) do
     Channel.earliest_timeout(state.channels)
-    |> calculate_next(state)
+    |> process_channel(state)
   end
 
-  defp calculate_next(nil, state) do
+  defp process_channel(nil, state) do
     count = Enum.count(state.channels)
     log(:info, state, "All #{count} dynamic channel(s) are active.")
-    @default_timeout
+    {@default_timeout, state}
   end
 
-  defp calculate_next(channel, state) do
+  defp process_channel(channel, state) do
     now = DateTime.utc_now()
     delta = DateTime.diff(channel.timeout, now, :millisecond)
 
     if delta <= 0 do
       log(:info, state, "Channel #{inspect(channel.name)} needs cleaning up now.")
-      {:continue, channel.id}
+      state = delete_channel(channel, state)
+      process_next_channel(state)
     else
       log(:info, state, "Next cleanup will be #{inspect(channel.name)} in #{delta} ms.")
-      delta
+      {delta, state}
     end
   end
 
-  defp delete_channel(state, id) do
-    channel = Enum.find(state.channels, &(&1.id == id))
+  defp delete_channel(channel, state) do
+    id = channel.id
     state = %State{state | channels: List.delete(state.channels, channel)}
 
     case Discord.delete_channel(id) do
